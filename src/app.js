0d9fafcbe5d7950cd5f146f0e9a548e73073d045
milindsahay <milind.sahay123@gmail.com>
2020-09-07 22:51:10 +0530

    Added hooks and search params, dropdown

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import  pet  from "./pet";
-
-const app = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(pet, {
-      name: "Leo",
-      animal: "Dog",
-      breed: "retriever",
-    }),
-    React.createElement(pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Coackatiel",
-    }),
-    React.createElement(pet, { name: "Luna", animal: "Dog", breed: "Pug" })
+import React from "react";
+import { render } from "react-dom";
+//import Pet from "./pet";
+import SearchParams from "./SearchParams";
+const App = () => {
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Leo",
+  //     animal: "Dog",
+  //     breed: "retriever",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Coackatiel",
+  //   }),
+  //   React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Pug" })
+  // );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
   );
 };
 
-render(React.createElement(app), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
