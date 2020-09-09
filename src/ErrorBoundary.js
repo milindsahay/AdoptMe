170f94948bda0c8c1bac1135f816c90a992cf979
milindsahay <milind.sahay123@gmail.com>
2020-09-10 01:51:19 +0530

    added error boundary, modals and contexts

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Redirect, Link } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      redirect: false,
+    };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("Error Boundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ redirect: true });
+      }, 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing <Link to="/">Click here</Link> to
+          get back to homepage
+        </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
+import ThemeContext from "./ThemeContext";
 //import Pet from "./pet";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 const App = () => {
+  const themeHook = useState("peru");
+
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
